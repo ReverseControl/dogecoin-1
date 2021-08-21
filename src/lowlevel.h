4aef3b46b00206d5e57efe2603c5caf66852d280
Escanor Liones <ReverseControl@users.noreply.github.com>
2021-08-21 16:56:46 -0400

    [ lowlevel ] Header file for wizardry and the dark arts.

---
+++ src/lowlevel.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2021 The Dogecoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+//Here be dragons, wizards, witches and master of the dark arts. 
+//Careful thou who wonders 'round these parts without magic spells,
+//mana potions and doggy treats...especially magical doggy treats.
+
+#ifndef LOWLEVEL_H
+#define LOWLEVEL_H
+
+#ifdef _WIN32
+#define __NT__
+    #include <intrin.h>
+#else
+    #include <x86intrin.h>
+#endif
+
+#include <cpuid.h>
+
+/* \union Useful for buffers that will need to be hashed
+ *        where data gather comes from different int types.       
+ */
+union block_512bits {
+    char                    byte[64];
+    unsigned char           ubyte[64];
+    uint16_t                word[32];
+    uint32_t                dword[16];
+    uint64_t                qword[8];
+    unsigned long long int  ulli[8]; 
+};
+
+
+
+#endif  /* LOWLEVEL_H */
